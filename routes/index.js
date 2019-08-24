1d976a14edd2a8892cdd1db3e087a083dc7e5fee
Ccastillo06 <ccastillon92@gmail.com>
2019-08-24 12:29:50 +0200

    Add dotenv, debug and cors configs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json({
+    name: process.env.NAME || 'No name set',
+  })
 });
 
 module.exports = router;
