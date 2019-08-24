8456e48d4843c577d708cfb3ea64d9416d8477e4
Ccastillo06 <ccastillon92@gmail.com>
2019-08-24 12:45:00 +0200

    Config prettier and eslint for the project

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.status(200).json({
-    name: process.env.NAME || 'No name set',
-  })
+    name: process.env.NAME || 'No name set',
+  });
 });
 
 module.exports = router;
